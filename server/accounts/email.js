3f12bb7383c6907bcb2e06e17b74a0dbf7664bca
Jaired Jawed <jairedjawed@Jaireds-MacBook-Pro.local>
2018-09-06 22:00:20 -0700

    Removed unnecessary variables

--- server/accounts/email.js
+++ server/accounts/email.js
@@ -5,7 +5,7 @@ Accounts.emailTemplates.verifyEmail = {
   subject() {
     return '[IBSayin\'] Verify Your Email Address';
   },
-  text( user, url ) {
+  text(user, url) {
     let emailAddress   = user.emails[0].address,
         urlWithoutHash = url.replace( '#/', '' ),
         supportEmail   = 'support@ibsayin.tk',
@@ -19,9 +19,8 @@ Accounts.emailTemplates.resetPassword = {
   subject() {
     return '[IBSayin\'] Reset Your Password';
   },
-  text( user, url ) {
-    let emailAddress   = user.emails[0].address,
-        urlWithoutHash = url.replace( '#/', '' ),
+  text(user, url) {
+        let urlWithoutHash = url.replace( '#/', '' ),
         supportEmail   = 'support@ibsayin.tk',
         emailBody      = `To reset your password, visit the following link:\n\n${urlWithoutHash}\n\n If you did not request this link, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
 
